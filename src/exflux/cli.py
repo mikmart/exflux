d6a01880beaf219f59257e64bb90838d757f9b61
Mikko Marttila <13412395+mikmart@users.noreply.github.com>
2024-10-01 19:43:40 +0100

    Add timestamps to log messages

--- src/exflux/cli.py
+++ src/exflux/cli.py
@@ -1,6 +1,11 @@
 import argparse
+import time
 
 from .settings import Settings, create_export_destination, create_exporter
+
+
+def log(message):
+    print(f"[{time.strftime('%H:%M:%S')}] {message}")
 
 
 def parse_args():
@@ -11,10 +16,11 @@ def parse_args():
 
 def main():
     args = parse_args()
+    log(f"Reading export settings from {args.settings_file}...")
     settings = Settings.load(args.settings_file)
     exporter = create_exporter(settings.database)
     for export in settings.exports:
-        print(f"Exporting from {export.source.bucket} to {export.destination.name}...")
+        log(f"Exporting from {export.source.bucket} to {export.destination.name}...")
         destination = create_export_destination(export.destination)
         exporter.export(export.source.bucket, export.source.query, destination)
 
